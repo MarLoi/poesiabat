338e04163ac57bdcf69e5e97c74e502b7533c81a
MarLoi <73001509+MarLoi@users.noreply.github.com>
2022-05-13 09:14:13 +0200

    Update automatikoa_tw.R

--- automatikoa_tw.R
+++ automatikoa_tw.R
@@ -8,38 +8,38 @@ orain = as.POSIXct(Sys.time(),format="%m/%d/%Y %H:%M:%S")
 write(as.character(orain), file="log.txt",append=TRUE)
 
 
-# # whatever name you assigned to your created app
-# appname <- "poesiabat"
-# 
-# ## api key (example below is not a real key)
-# key <- "2C2NmWG7kie43GW8JzA0fckjM"
-# 
-# ## api secret (example below is not a real key)
-# secret <- "GDxSOgk1pdV7Zk9ykPObdRK91bAmtpyppmMxgouYNq8Zq3pnPG"
-# 
-# twitter_token <- create_token(
-#   app = appname,
-#   consumer_key = key,
-#   consumer_secret = secret,
-#   access_token = "1524375667365888000-ZPnskvY3ZsKJYUNwy4uIoZDQkc3eeU",
-#   access_secret = "AqzctjmFaRvPq1aJI3bpGGuXGX4lwvcZplGRX0HbSOsAD")
-
-
 # whatever name you assigned to your created app
-appname <- "sareak probatzen"
+appname <- "poesiabat"
 
 ## api key (example below is not a real key)
-key <- "1hFVQmG7WrtRGekqAaUz5yAWj"
-
+key <- "2C2NmWG7kie43GW8JzA0fckjM"
+# 
 ## api secret (example below is not a real key)
-secret <- "dL25UesAQHuWfYpKDkgk05IdN7ZUwFbImUNN4qbw6vfbdnPZeD"
-
+secret <- "GDxSOgk1pdV7Zk9ykPObdRK91bAmtpyppmMxgouYNq8Zq3pnPG"
+# 
 twitter_token <- create_token(
   app = appname,
   consumer_key = key,
   consumer_secret = secret,
-  access_token = "2873995706-hJb5EmtpUJLOeR2BrG2F1WKEEpasynEetOIcHOE",
-  access_secret = "RnbguBacjdDgCk5ZOZymfMzRvccvdFUYFE3QlXGa5QuLA")
+  access_token = "1524375667365888000-ZPnskvY3ZsKJYUNwy4uIoZDQkc3eeU",
+  access_secret = "AqzctjmFaRvPq1aJI3bpGGuXGX4lwvcZplGRX0HbSOsAD")
+
+
+## whatever name you assigned to your created app
+#appname <- "sareak probatzen"
+
+## api key (example below is not a real key)
+#key <- "1hFVQmG7WrtRGekqAaUz5yAWj"
+
+## api secret (example below is not a real key)
+#secret <- "dL25UesAQHuWfYpKDkgk05IdN7ZUwFbImUNN4qbw6vfbdnPZeD"
+
+#twitter_token <- create_token(
+#  app = appname,
+#  consumer_key = key,
+#  consumer_secret = secret,
+#  access_token = "2873995706-hJb5EmtpUJLOeR2BrG2F1WKEEpasynEetOIcHOE",
+#  access_secret = "RnbguBacjdDgCk5ZOZymfMzRvccvdFUYFE3QlXGa5QuLA")
 
 
 rstats_tweets <- search_tweets(q = "#poesiabat", n = 10)
@@ -118,7 +118,7 @@ if(nrow(rstats_tweets_filt)==0){
     
     # TWEETA ------------------------------------------------------------------
     
-    #post_tweet(emaitza)
+    post_tweet(emaitza)
     print(emaitza)
   
   }
